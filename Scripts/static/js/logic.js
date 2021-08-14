549e90b57bc46359a963d73e4001b7f38dedc347
DataTortuga <dcurtis390@gmail.com>
2021-08-14 14:06:06 -0500

    Finishing touches on website

--- Scripts/static/js/logic.js
+++ Scripts/static/js/logic.js
@@ -11,7 +11,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var url = "http://127.0.0.1:5000/api/movies"
 
 d3.json(url).then(function (data) {
-    console.log("api test", data)
+    // console.log("api test", data)
 
 
     for (i = 0; i < data.length; i++) {
@@ -39,9 +39,9 @@ d3.select("#genre").on("change", updateBarchart)
 
 function updateBarchart() {
     let selected_genre = d3.select("#genre").property("value")
-    console.log(selected_genre)
+    // console.log(selected_genre)
     let url = "http://127.0.0.1:5000/" + selected_genre.split(",")[0]
-    console.log(url)
+    // console.log(url)
     d3.json(url).then(function (data) {
         xlabel = data.map(a => a.country)
         ydata = data.map(a => a.avg_rating)
@@ -64,7 +64,7 @@ function getColor(d) {
 
 d3.json("http://127.0.0.1:5000/Crime").then(function (data) {
 
-    console.log(data)
+    // console.log(data)
     xlabel = data.map(a => a.country)
     ydata = data.map(a => a.avg_rating)
     bgcolors = data.map(a => getColor(a.avg_rating))
@@ -98,12 +98,12 @@ d3.json("http://127.0.0.1:5000/Crime").then(function (data) {
 
 d3.json("http://127.0.0.1:5000/country/Albania").then(function (data) {
 
-    console.log(data)
+    // console.log(data)
     xlabel = data.map(a => a.genre)
     ydata = data.map(a => a.nmovies)
-    bgcolors = data.map(a => getColor(a.nmovies))
+    bgcolors = data.map(a => getColor2(a.nmovies))
     bdrcolors = data.map(a => 'rgba(153, 102, 255, 1)')
-    console.log(ydata)
+    // console.log(ydata)
     var ctx = document.getElementById('myChart2').getContext('2d');
     myChart2 = new Chart(ctx, {
         type: 'bar',
@@ -129,6 +129,35 @@ d3.json("http://127.0.0.1:5000/country/Albania").then(function (data) {
         }
     });
 })
+
+
+d3.select("#country").on("change", updateBarchart2)
+
+function updateBarchart2() {
+    let selected_country = d3.select("#country").property("value")
+    // console.log(selected_country)
+    let url = "http://127.0.0.1:5000/" + 'country/' + selected_country
+    console.log(url)
+
+
+    d3.json(url).then(function (data) {
+        xlabel = data.map(a => a.genre)
+        ydata = data.map(a => a.nmovies)
+        bgcolors = data.map(a => getColor2(a.nmovies))
+        bdrcolors = data.map(a => 'rgba(153, 102, 255, 1)')
+        myChart2.data.datasets[0].data = ydata
+        myChart2.data.datasets[0].backgroundColor = bgcolors
+        myChart2.data.labels = xlabel
+        myChart2.update()
+    })
+
+}
+
+function getColor2(d) {
+    return d > 300 ? 'green' :
+        d > 100 ? 'yellow' :
+            d > 10 ? 'red' : 'darkred';
+}
 
 var myChart
 var myChart2
